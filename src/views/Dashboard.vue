3b20584e83c32b5894caeae45e7ac90365560529
JustEd / GiSpatial / Rizq Solutions <react7tech@gmail.com>
2021-08-07 01:47:38 +0800

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  <iframe src="https://etiqatracker.vercel.app/" height="900" width="100%" title="Iframe Example"></iframe>
+  <iframe src="https://etiqa-ncov-track.vercel.app/" height="900" width="100%" title="Iframe Example"></iframe>
   </div>
 </template>
 <style></style>
