b7bf6d4a6213510f4036eeb5b208d5903f25984e
JustEd <ed@rizq.us>
2021-08-06 15:39:28 +0800

    assessment

---
+++ src/layout/ContentFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="footer px-4">
+    <div class="row align-items-center justify-content-lg-between">
+      <div class="col-lg-6">
+        <div class="copyright text-center text-lg-left text-muted">
+          © {{ year }}
+          <a
+            href="https://www.creative-tim.com"
+            class="font-weight-bold ml-1"
+            target="_blank"
+            >GiSpatialTech (JustEd)</a
+          >
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <ul
+          class="nav nav-footer justify-content-center justify-content-lg-end"
+        >
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+<style></style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+  <iframe src="https://etiqatracker.vercel.app/" height="900" width="100%" title="Iframe Example"></iframe>
+  </div>
+</template>
+<style></style>
